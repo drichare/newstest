2d1ef2969f689bc64293625a25c35b1ce7038f44
drichare <dorichare.hp@gmail.com>
2025-08-29 14:37:48 +0900

    Update content.js

--- news/content.js
+++ news/content.js
@@ -1,13 +1,15 @@
 const newsData = [
-{ title: "hello", date: "2025/08/29", link: "item/2025/1/index.html", image: "item/2025/1/img/情報量が.jpg" },
-
-    { title: "べるだあ・ロボくん・ゆれん加入のお知らせ", date: "2025/08/23", link: "item/2025/9/index.html", image: "item/2025/9/img/2gen.png" },
-    { title: "グッズ販売のお知らせ", date: "2025/08/23", link: "item/2025/8/index.html", image: "item/2025/8/img/gods.png" },
-    { title: "新メンバー2名加入のおしらせ", date: "2025/08/06", link: "item/2025/7/index.html", image: "item/2025/7/img/2memj.png" },
-    { title: "新メンバー加入のお知らせ", date: "2025/07/19", link: "item/2025/6/index.html", image: "item/2025/6/img/newmem.png" },
-    { title: "ドリチャレ活動1周年", date: "2025/06/16", link: "item/2025/5/index.html", image: "img/1st.jpg" },
-    { title: "ドリチャレオンライン新商品追加お知らせ", date: "2025/05/09", link: "item/2025/4/index.html", image: "img/shop.png" },
-    { title: "不具合の修正について", date: "2025/05/09", link: "item/2025/3/index.html", image: "img/3.PNG" },
-    { title: "裏方募集のおしらせ", date: "2025", link: "item/2025/2/index.html", image: "img/ura.png" },
-    { title: "ドリチャレ公式HP開設", date: "2025", link: "item/2025/1/index.html", image: "img/rinku.png" }
-];
+{ title: "tesut", date: "2025/08/29", link: "item/2025/2/index.html", image: "item/2025/2/img/情報量が.jpg" },
+
+{ title: "hello", date: "2025/08/29", link: "item/2025/1/index.html", image: "item/2025/1/img/情報量が.jpg" },
+
+    { title: "べるだあ・ロボくん・ゆれん加入のお知らせ", date: "2025/08/23", link: "item/2025/9/index.html", image: "item/2025/9/img/2gen.png" },
+    { title: "グッズ販売のお知らせ", date: "2025/08/23", link: "item/2025/8/index.html", image: "item/2025/8/img/gods.png" },
+    { title: "新メンバー2名加入のおしらせ", date: "2025/08/06", link: "item/2025/7/index.html", image: "item/2025/7/img/2memj.png" },
+    { title: "新メンバー加入のお知らせ", date: "2025/07/19", link: "item/2025/6/index.html", image: "item/2025/6/img/newmem.png" },
+    { title: "ドリチャレ活動1周年", date: "2025/06/16", link: "item/2025/5/index.html", image: "img/1st.jpg" },
+    { title: "ドリチャレオンライン新商品追加お知らせ", date: "2025/05/09", link: "item/2025/4/index.html", image: "img/shop.png" },
+    { title: "不具合の修正について", date: "2025/05/09", link: "item/2025/3/index.html", image: "img/3.PNG" },
+    { title: "裏方募集のおしらせ", date: "2025", link: "item/2025/2/index.html", image: "img/ura.png" },
+    { title: "ドリチャレ公式HP開設", date: "2025", link: "item/2025/1/index.html", image: "img/rinku.png" }
+];
